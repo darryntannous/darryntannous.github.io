a2603d121bd3b0e318d00171b970f6df568bd387
Darryn Tannous <darryntannous@gmail.com>
2023-03-21 14:40:54 -0400

    Secondary commit

---
+++ script.js
@@ -0,0 +1,11 @@
+//Click button
+const button = document.getElementById ("btn");
+{
+ button.addEventListener("click", myFunction)
+
+   function myFunction()
+   {
+     document.body.style.backgroundColor = "blue";
+
+    }
+}  
